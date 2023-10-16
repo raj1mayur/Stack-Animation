13b889587e7e4a66b2db97ed6f742edb678d5574
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2023-10-16 18:55:38 +0530

    Clouds going in different directions .The cloud image is in public folder.

--- animation/src/App.js
+++ animation/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Globe from './components/char animation/globe';
+// import Globe from './components/char animation/globe';
+import Cloud from './components/cloud animation/cloud';
 
 function App() {
   return (
-   <Globe head="Character"/>
+  //  <Globe head="Character"/>,
+   <Cloud />
   );
 }
 
---
+++ animation/src/components/cloud animation/cloud.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+import "./cloud.css";
+
+export default function Cloud(){
+    return(
+        <>
+           <div className="cloud">
+            <img className="animeCloud cl0"  src="/images/cloudpng1.webp" alt="cloud" />
+            <img className="animeCloud cl1"  src="/images/cloudpng1.webp" alt="cloud" />
+            <img className="animeCloud cl2"  src="/images/cloudpng1.webp" alt="cloud" />
+           </div>
+
+
+        </>
+    )
+}
